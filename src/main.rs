b85a7f0203365df21dee27ca7c04de0a9cc0fdfc
dvvvvvv <dvvvvvv@dvvvvvv.com>
2020-02-27 00:08:15 +0900

    add ip version

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use hyper::body::{self, Body};
 use hyper::client::{Client, HttpConnector};
 use hyper::http::Uri;
-use hyper::body::{self, Body};
 use hyper_tls::HttpsConnector;
-use std::str::FromStr;
 use std::convert::From;
+use std::str::FromStr;
 
 #[derive(Debug)]
 enum Error {
@@ -19,15 +19,15 @@ impl From<hyper::error::Error> for Error {
 impl std::error::Error for Error {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
-            Error::HyperError(hyper_error) => Some(hyper_error)
+            Error::HyperError(hyper_error) => Some(hyper_error),
         }
     }
 }
 
 impl std::fmt::Display for Error {
-    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) ->  std::fmt::Result {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::HyperError(hyper_error) => hyper_error.fmt(formatter)
+            Error::HyperError(hyper_error) => hyper_error.fmt(formatter),
         }
     }
 }
@@ -43,7 +43,14 @@ async fn main() {
     println!("{}", ip);
 }
 
-fn client() -> Client<HttpsConnector<HttpConnector>, Body>{
-    Client::builder().build::<_, Body>(hyper_tls::HttpsConnector::new())
+enum IpVersion {
+    V4,
+    V6,
 }
 
+//async fn ip(client: Client<_,Body>, ip_version: IpVersion) -> String {
+//}
+
+fn client() -> Client<HttpsConnector<HttpConnector>, Body> {
+    Client::builder().build::<_, Body>(hyper_tls::HttpsConnector::new())
+}
