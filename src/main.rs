b7f1d96b6bb2ada25ab6e03201e19a65c8f7ef41
dvvvvvv <dvvvvvv@dvvvvvv.com>
2020-02-27 00:07:46 +0900

    add error type

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,34 @@ use hyper::http::Uri;
 use hyper::body::{self, Body};
 use hyper_tls::HttpsConnector;
 use std::str::FromStr;
+use std::convert::From;
+
+#[derive(Debug)]
+enum Error {
+    HyperError(hyper::error::Error),
+}
+
+impl From<hyper::error::Error> for Error {
+    fn from(error: hyper::error::Error) -> Error {
+        Error::HyperError(error)
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error::HyperError(hyper_error) => Some(hyper_error)
+        }
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) ->  std::fmt::Result {
+        match self {
+            Error::HyperError(hyper_error) => hyper_error.fmt(formatter)
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
