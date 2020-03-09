9ca0f1d5075226661b0bdc143879455628863805
dvvvvvv <dvvvvvv@dvvvvvv.com>
2020-03-09 11:58:58 +0900

    separate error.rs

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,11 @@
 use clap::{App, Arg};
-use std::convert::From;
 use std::str::FromStr;
 
 use client::{IpVersion, IpifyClient};
+pub use error::{Error, Result};
 
 mod client;
-
-type Result<T> = std::result::Result<T, Error>;
-
-#[derive(Debug)]
-pub enum Error {
-    HyperError(hyper::error::Error),
-    UnsupportedIpVersion(String),
-}
-
-impl From<hyper::error::Error> for Error {
-    fn from(error: hyper::error::Error) -> Error {
-        Error::HyperError(error)
-    }
-}
-
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            Error::HyperError(hyper_error) => Some(hyper_error),
-            Error::UnsupportedIpVersion(_) => None,
-        }
-    }
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Error::HyperError(hyper_error) => hyper_error.fmt(formatter),
-            Error::UnsupportedIpVersion(version) => {
-                write!(formatter, "UnsupportedIpVersion({})", version)
-            }
-        }
-    }
-}
+mod error;
 
 #[tokio::main]
 async fn main() {
